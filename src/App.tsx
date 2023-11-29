d2aa0b4a1a911ea2423bcb3cc13ccf4436b85441
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-29 20:24:44 -0300

    chore: Adjusting app file location.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
