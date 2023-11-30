b51ddd5b8f660006f2c4324815ad740215bae950
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-29 23:11:11 -0300

    feat: Add new colors.

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,14 @@
 export const colors = {
   background: {
     white: '#FFFFFF',
+    grayExtraLight: '#F2F2F2',
   },
-
+  text: {
+    white: '#FFFFFF',
+    black: '#000000',
+    grayLight: '#B5B9C4',
+    grayMedium: '#747476',
+  },
   cardBackgrounds: {
     alive: '#8BBE8A',
     unknown: '#417D9A',
