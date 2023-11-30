f3740d02740ebd87111c18eca36d1b82ec4e9c65
Amanda Duarte <amandaduuaarte@gmail.com>
2023-11-29 23:20:49 -0300

    feat: Create textfield component.

---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+import {colors} from '../../utils/colors';
+
+export const Container = styled.View`
+  height: 45px;
+  width: 100%;
+  border-radius: 12px;
+  background: ${colors.background.grayExtraLight};
+  padding: 6px 12px;
+  justify-content: row;
+`;
+
+export const Input = styled.TextInput`
+  height: 100%;
+  width: 90%;
+  font-size: 14px;
+  font-family: 'Roboto-medium';
+  align-self: flex-end;
+  color: ${colors.text.grayMedium};
+`;
