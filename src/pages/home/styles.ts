6fd393c9535d9477b4161d0c82f9ce81635a9fde
Amanda Duarte <amandaduuaarte@gmail.com>
2023-12-01 20:51:54 -0300

    feat: Add home list.

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,25 @@
 import styled from 'styled-components/native';
+import {colors} from '../../utils/colors';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  padding: 12px;
+  padding: 24px;
+  margin-top: 32px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  font-family: 'Roboto-bold';
+  color: ${colors.text.black};
+`;
+
+export const Description = styled.Text`
+  font-size: 16px;
+  font-family: 'Roboto-regular';
+  color: ${colors.text.grayMedium};
+  margin: 12px 0;
+`;
+
+export const Content = styled.View`
+  margin-top: 24px;
 `;
