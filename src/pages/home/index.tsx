dd9c377e4f51a235039ffe654e08cecf842bd231
Amanda Duarte <amandaduuaarte@gmail.com>
2023-12-01 20:51:54 -0300

    feat: Add home list.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {allcharacters} from '../../queries';
+import React, {useState} from 'react';
+import {FlatList, SafeAreaView} from 'react-native';
+import {ALL_CHARACTERS} from '../../queries';
 import {useQuery} from '@apollo/client';
 
-import {Container} from './styles';
-import {Card} from '../../components/Card';
+import {Container, Content, Description, Title} from './styles';
+import {Card, TextField} from '../../components';
 
 export const Home: React.FC = () => {
-  const {data} = useQuery(allcharacters);
+  const [value, setValue] = useState<string>('');
+  const {data} = useQuery(ALL_CHARACTERS);
 
-  console.log('aqiiiiiii', data);
-
+  console.log(data);
   return (
     <Container>
-      <Text>Hello world</Text>
-      {/* <Card /> */}
-      {/* {data && (
-        <FlatList
-          data={data.characters.results}
-          renderItem={({item}) => <Text>{item.name}</Text>}
-          keyExtractor={item => item.id}
+      <SafeAreaView>
+        <Title>Rick And Morty</Title>
+        <Description>
+          App for see more about Rick and Morty characthers
+        </Description>
+        <TextField
+          placeholder="What character you loking for?"
+          callBack={e => setValue(e)}
+          value={value}
         />
-      )} */}
+        <Content>
+          {data && (
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={data.characters.results}
+              renderItem={({item}) => <Card key={item.id} data={item} />}
+              keyExtractor={item => item.id}
+            />
+          )}
+        </Content>
+      </SafeAreaView>
     </Container>
   );
 };
