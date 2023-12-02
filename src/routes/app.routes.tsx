f1a07869c50db21ec3aaaa58a7900695921f14f5
Amanda Duarte <amandaduuaarte@gmail.com>
2023-12-01 21:03:57 -0300

    feat: Create route file.

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home} from '../pages/home';
+import {Details} from '../pages/details';
+
+export const Routes = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
