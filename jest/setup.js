c7b646ff222a501127753432d81aa55227a6e516
Amanda Duarte <amandaduuaarte@gmail.com>
2023-12-12 19:42:48 -0300

    feat: add new test for Card component

---
+++ jest/setup.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+// include this line for mocking react-native-gesture-handler
+import 'react-native-gesture-handler/jestSetup';
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
